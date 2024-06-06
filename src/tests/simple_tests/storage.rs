ac9744638662f7b1d701207291ff7695c75afd79
Vladislav Volosnikov <vladyan18@gmail.com>
2024-06-06 09:34:49 +0200

    feat: cleanup, fix and enhance log asm tests (#161)

--- src/tests/simple_tests/storage.rs
+++ src/tests/simple_tests/storage.rs
@@ -5,7 +5,7 @@ mod tests {
     #[test_log::test]
     fn test_pubdata_and_storage_writes() {
         run_asm_based_test(
-            "src/tests/simple_tests/testdata/storage_writes",
+            "src/tests/simple_tests/testdata/log/storage/storage_writes",
             &[],
             Options {
                 cycles_per_vm_snapshot: 1,
@@ -17,7 +17,7 @@ mod tests {
     #[test_log::test]
     fn test_storage_reads() {
         run_asm_based_test(
-            "src/tests/simple_tests/testdata/storage_reads",
+            "src/tests/simple_tests/testdata/log/storage/storage_reads",
             &[],
             Options {
                 // Do only 1 cycle per VM snapshot to really test all the boundary conditions.
@@ -28,11 +28,12 @@ mod tests {
     }
 
     #[test_log::test]
-    fn test_pubdata_refunds() {
+    fn test_storage_pubdata_refunds() {
         run_asm_based_test(
-            "src/tests/simple_tests/testdata/pubdata_refunds",
+            "src/tests/simple_tests/testdata/log/storage/storage_pubdata_refunds",
             &[],
             Options {
+                // Do only 1 cycle per VM snapshot to really test all the boundary conditions.
                 cycles_per_vm_snapshot: 1,
                 ..Default::default()
             },
